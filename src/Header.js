fb48d3807c39f03e600b37cb42b14aadd93affdd
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-04-04 01:17:38 -0300

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Headers (){
     return(
         <header>
-            <img className="hdr_img" src="../images/meme.png" alt='erro na imagem'/>
+            <img className="hdr_img" src="./images/meme.png" alt='erro na imagem'/>
             <div className="hdr_part2">
                 <span className="hdr_title">Meme Generator</span>
                 <span className="hdr_subtitle">React Course - Project 4</span>
